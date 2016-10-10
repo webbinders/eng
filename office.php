51d33cc8a004823d15988bb88b287be430225bef
Gal_Sergey <galsergey20@gmail.com>
2016-10-10 22:07:16 +0300

    Отработало регистрацию нового пользователя, если данные введены корректноЭ

--- office.php
+++ office.php
@@ -1,7 +1,7 @@
 <?php
         session_start();
         include './classes/pageClass.php'; //подключаем файл класса страницы
-        $pageObj = new pageClass;
+        $pageObj = new pageClass('выводим код офиса');
         //если пользователь входит впервые, предложить войти или зарегистрироваться
         //если пользователь вошел дать ему знать
         echo $pageObj->build_Page();
