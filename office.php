a73b10093cad82ee80922a3e9ad6b53284442323
Gal_Sergey <galsergey20@gmail.com>
2017-06-09 18:42:31 +0300

    Теперь если поле с количеством новых фопросов пустое, будет считаться, что новых вопросов 0.

--- office.php
+++ office.php
@@ -78,7 +78,8 @@ var_dump($dictionary);echo '<br>';*/
                
             foreach ($dictionary->wordsList as $word) {
                 //частота слова в тексте
-                $frequency=$dictionary->frequencyMap[$word->foreign];
+                //var_dump($dictionary->wordFrequencyMap);
+                $frequency=$dictionary->wordFrequencyMap[htmlentities($word->foreign)];
                // var_dump($dictionary);
                 //если пользователь не смотрел перевод слова, считается что он его знает
                 if($word->stud ==0 ||( $word->stud == 1 && time() -$word->lastData > $CRITERION_OF_REPETITION)){
@@ -106,11 +107,48 @@ var_dump($dictionary);echo '<br>';*/
         if (isset($_POST['btn_find']) && ($_POST['word'] != '')){
             //include 'find_word.php';
             
+            //получаем слово как объект из строки текстового поля
             @$word = getWordObjFromString($_POST['word'], $dictionary);
+            //получаем перевод слова
             $translate = $word->getNative();
+            //добавляем слово в список для изучения
             $word = $word->addToStudList();
+            //Вставляем перевод в тестовое поле
+            $_POST['trans_area'] = $translate;  
+            
+            $wordList = new WordList($_POST['word']);
+            
+            //Добавляем id примера в поле "Примеры" слов, входящих в пример
+            if(sizeof($wordList->wordsList) > 1){
+                foreach ($wordList->wordsList as $key => $subword) {
+                    //из строки поля "примеры" создаем массив идентификаторов
+                    ///создаем запрос на чтение поля "примеры"
+                    $id = $subword->id;
+                    $query = "SELECT `examples` FROM `thesaurus` WHERE id = $id";
+                    $result = queryRun($query,'Ошибка при чтении поля examples');
+                    ///получаем строку
+                    $row = mysql_fetch_row($result);
+                    $exampleStr = $row[0];
+                    ///делаем из нее массив
+                    $idArr = array();
+                    if (strlen($exampleStr)>0){
 
-            $_POST['trans_area'] = $translate;
+                        $idArr = explode(',', $exampleStr);
+                    }
+                    //вставляем в этот массив идентификатор примера
+                    if (!in_array($word->id, $idArr))
+                    $idArr[$id]=$word->id;  
+                    //преобразуем массив в строку
+                    $exampleStr = implode(',', $idArr);
+                    //обновляем тезарус
+                    $query = "UPDATE `thesaurus` SET `examples`= '$exampleStr' WHERE `id` = $id;";
+                   
+                    $result = queryRun($query,'Ошибка при обновлении ( UPDATE ) поля examples');
+                }
+
+            }
+            
+
 
         }
         //если нажата кнопка добавить перевод
@@ -121,11 +159,11 @@ var_dump($dictionary);echo '<br>';*/
             $_POST['word']='';
         }
         include_once 'forms/reading_form.php';//подключаем файл формы для чтения
-        if (isset($_POST['btn_add']) || isset($_POST['btn_handling_text'])){
+       /* if (isset($_POST['btn_add']) || isset($_POST['btn_handling_text'])){
             $my_form->delInputForm($trans_area);
             $my_form->delInputForm($btn_add);
             
-        }
+        }*/
         if (isset($dictionary))
         $_SESSION['dictionary'] = serialize ($dictionary);
                 
@@ -146,6 +184,7 @@ var_dump($dictionary);echo '<br>';*/
         if(isset($_POST['btn_start_stud'])){
             //если нажата кнопка начать изучение
             //загружаем список для изучения
+            if ($_POST['newQuestions']=='') $_POST['newQuestions']=0;
             $studList = new StudList($_POST['newQuestions']); 
 
             //
@@ -166,6 +205,12 @@ var_dump($dictionary);echo '<br>';*/
         }
         if(isset($_POST['btn_wrong'])){
             $button = 'btn_wrong';
+        }
+        if(isset($_POST['btn_view_example'])) {
+            $button = 'btn_view_example';
+        }
+        if (isset($_POST['btn_show_native'])){
+            $button = 'btn_show_native';
         }
         //если список для изучения не пуст
         if(isset($studList) && sizeof($studList->studList)) {
@@ -245,9 +290,38 @@ function testing($studList, $button){
                 if($native!=''){
                     $_POST['answer_text_area'] = $native;
                 }
+                
+                //Находим id примеров содержащих текущее слово                   
+                $idArr = $currentWord->findExamples($currentWord->foreign);  
+                //удаляем из списка примеров id текущего слова, т.к. метод findExamples включает и само слово в спимок примеров
+                if (in_array($currentWord->id, $idArr)) {
+                    foreach ($idArr as $key => $value) {
+                        if ($idArr[$key]==$currentWord->id){
+                            unset($idArr[$key]);
+                            break;
+                        }
+                    }
+                }
+                        
+                if(sizeof($idArr)){
+                    //Для каждого элемента массива содержащего id  примеров
+                    foreach ($idArr as $valueId) {
+                        //Создаем объект exampleObj класса Word
+                        $property_arr['id'] = $valueId;
+                        $exampleObj = new Word($property_arr);
+                        //Добавить его в список примеров
+                        $exampleList[$valueId]=$exampleObj;
+                    }
+                }
+                
+                //Сериализируем список
+                if (isset($exampleList)) $_SESSION['exampleList'] = serialize($exampleList);
+                
              
                 $_SESSION['currentWord'] = $currentWord->foreign;
+                echo '<br> examplesList :'.$currentWord->examples.'<br>';
                 break;
+                
             //---------------------------------------------   
             case 'btn_right'://нажата кнопка "Правильно"   |
             //---------------------------------------------
@@ -296,7 +370,9 @@ function testing($studList, $button){
                 
                 $result = queryRun($query,'Error in time update '.$tableName);
                 //тезарус
-                $query = "UPDATE `thesaurus` SET `foreign`='".addslashes($currentWord->foreign)."',`native`='".addslashes($currentWord->native)."' WHERE `id`=$currentWord->id";
+                $foreign = addslashes($currentWord->foreign);
+                $native = addslashes($currentWord->native);
+                $query = "UPDATE `thesaurus` SET `foreign`='$foreign',`native`='$native' WHERE `id`=$currentWord->id";
                 $result = queryRun($query,'Error in time update thesaurus');
                 
                 //удаляем слово из списка для изучения на сегодня
@@ -342,8 +418,12 @@ function testing($studList, $button){
                 
                 $result = queryRun($query,'Error in time update '.$tableName);
                 //тезарус
-                $query = "UPDATE `thesaurus` SET `foreign`='$currentWord->foreign',`native`='$currentWord->native' WHERE `id`=$currentWord->id";
+                $foreign = addslashes($currentWord->foreign);
+                $native = addslashes($currentWord->native);
+                $query = "UPDATE `thesaurus` SET `foreign`='$foreign',`native`='$native' WHERE `id`=$currentWord->id";
+                //echo $query;
                 $result = queryRun($query,'Error in time update thesaurus');
+                
 /*echo 'previos<br>';
                 $prevWord = prev($studList->studList);
                 var_dump($prevWord);
@@ -369,6 +449,71 @@ function testing($studList, $button){
                 $_SESSION['currentWord'] = $currentWord->foreign;
                 $_POST['question_text_area']=$currentWord->foreign;
                 $_POST['answer_text_area']='';
+                break;
+            //-----------------------------------
+            case 'btn_view_example'://Если нажата "Показать примеры"
+            //-----------------------------------
+                //копируем значения текстовых областей в свойства
+                $currentWord->foreign = $_POST['question_text_area'];  
+                $currentWord->native = $_POST['answer_text_area']; 
+                
+                //Саздать запрос на поиск примеров содержащих текущее слово
+               /* $foreign = trim(strtoupper(addslashes($_POST['question_text_area'])));
+                //echo $foreign;
+                $query = "SELECT `examples` FROM `thesaurus` WHERE `foreign`= '$foreign';";
+                $result = queryRun($query,'Error in time read  thesaurus');
+                $row = mysql_fetch_array($result);*/
+                
+                /*/Если строка содержащая список примеров не пустая
+                if(strlen($currentWord->examples)){
+                    //Превращаем ее в массив содержащий id примеров                    
+                    $idArr = explode(',', $currentWord->examples);
+                    //Для каждого элемента массива содержащего id  примеров
+                    foreach ($idArr as $valueId) {
+                        //Создаем объект exampleObj класса Word
+                        $property_arr['id'] = $valueId;
+                        $exampleObj = new Word($property_arr);
+                        //Добавить его в список примеров
+                        $exampleList[$valueId]=$exampleObj;
+                    }
+                    //Сериализируем список
+                    $exampleList = serialize($exampleList);
+                    $_SESSION['exampleList'] = $exampleList;
+                }
+                 * 
+                 */
+                
+
+                
+                break;
+            case 'btn_show_native': //Если была нажата "Показать перевод примера"
+                
+                //получаем из переменной список примеров
+                 $exampleList = unserialize($_SESSION['exampleList']);      
+                //определяем нажатую кнопку (пример с ее идентификатором надо добавить в список для изучения                
+                $btnId = key($_POST['btn_show_native']);
+                //Создаем объект exampleObj класса Word
+                $property_arr['id'] = $btnId;
+                $exampleObj = new Word($property_arr);
+                //добавляем пример в список для изучения
+                $exampleObj->addToStudList();
+                
+               /* if (isset($_POST['exampleList'])){
+                    var_dump($_POST['exampleList']);
+                $_POST['exampleList'] = unserialize($_POST['exampleList']);
+                $_POST['exampleList'] = serialize($_POST['exampleList']);  
+                }
+                 
+                if (isset($_POST['shown'])){
+                    
+                    $shownExample = unserialize ($_POST['shown']);
+                    print_r($shownExample);
+                }*/
+                //$_POST['shown'] = serialize($shownExample);
+                //создаем
+                
+
+                
                 break;
 
             default:
@@ -418,13 +563,53 @@ function getWordObjFromString($str,$dictionary){
     }
     else
         {//если слово взято не из текста (нет в списке слов)
-        $property_arr['foreign'] = $str;
+        //echo preg_match('/[а-яА-ЯёЁ]+/u', $str);
+        //не допускаем кириллических символов в поле для ввода иностранного слова        
+        if (!preg_match('/[а-яА-ЯёЁ]+/u', $str)){//если кирилических символов нет
+            //устанавливаем единственное известное свойство слова в массив свойств слова для создания объекта "слово"
+            $str = strtoupper(htmlentities($str));
+            $property_arr['foreign'] = qou($str);
+            //var_dump($property_arr);
+            //echo $property_arr['foreign'];
+            //создаем объект слово
+            $word = new Word ($property_arr);
+            //var_dump($word);
+        }
 
-        $word = new Word ($property_arr);
 
 
 
     }
+    //var_dump($word);
     return $word;    
+}
+/*
+ * Предназначена для восстановления кавычек в строке, после ее преобразования к верхнему регистру.
+ * Применение функций htmlentities или addslashes к строке вида 'Humans were created “in God’s image.”' ведет к непредсказуемым результататм.
+ * Такую строку надо обработать функцией htmlentities. В результате кавычки в ней преобразуются в последовательность символов вида  &ldquo;
+ * А вся строка вида 'Humans were created “in God’s image.”' примет вид Humans were created &ldquo;in God&rsquo;s image.&rdquo; * 
+ *  Теперь ее можно привести к верхнему регистру. Но последовательность сиволов обозначающие кавычки надо вернуть в нижний регистр
+ */
+function qou($str){
+    $start = 0;
+    $array_chang = array();
+    while(preg_match('/(&\w{5};)/', $str, $matches,PREG_OFFSET_CAPTURE,$start)){
+        $pos_apersand = $matches[1][1];
+        //echo 'pos_apersand ='.$pos_apersand.'<br>';
+        $array_chang[$pos_apersand]= strtolower(substr($str, $pos_apersand,7)) ;
+        $start =$pos_apersand+7;
+        //str2=substr($str, $pos_apersand+7);
+        
+        
+       
+    }
+    //var_dump($array_chang);
+    
+    foreach ($array_chang as $pos_apersand => $substr) {
+        
+        $str=substr_replace($str, $substr, $pos_apersand, strlen($substr));
+        
+    }
+    return $str;
 }
 ?>
