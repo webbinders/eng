69c2d200733568fed8d1152a9a4cf7618998c8fc
Gal_Sergey <galsergey20@gmail.com>
2017-10-29 22:33:43 +0200

    переписал класс страницы, чтобы можно было использовать шаблоны страниц и написал шаблон

--- classes/pageClass.php
+++ classes/pageClass.php
@@ -15,137 +15,93 @@
 class pageClass {
 
 
-    private $roof ;
+    private $pageHTML;
+    private $head;
+    private $header;
     private $content ;
     private $footer ;
+    private $templatePath = "../template.php";
     /*
      * Конструктор
      */
     function  __construct($_content = '', $socNets = array()){
-        $hello ='';
-        //если пользователь вошел 
-        if(isset($_SESSION['access']) && ($_SESSION['access'] == 1)){
-            $auto_button ="<button formaction='exit.php'>Выход</button>";
-            $hello = "<div>Hello ". $_SESSION['login'] ."</div>";            
-        }
-        else{
-            $auto_button =<<<BUTTON
-                        <button formaction='joining.php'>Вход</button>
-                        <button formaction='registration.php'>Регистрация</button>
-BUTTON;
-        }
-        //формируем гоизонтальное меню
-        $nav = <<<NAV
-        <nav>
-            <form>
-                <button formaction = './about.php'>О сайте</button>
-                <button formaction = './office.php'>Режим чтения</button>
-                <button formaction = './office.php'>Режим изучения</button>
-            </form>
-
-        </nav>
-NAV;
-        //формируем верхнюю часть страницы соответствующую свойству  $roof
-        $_roof = <<<ROOF
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Read in english</title>
-	
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<header>
-<div class="center-block">
-   <div class="logo">
-       <img src="logo.svg" width="50">
-       <p><i>Читайте с легкостью.</i></p>
-   </div>
-   <div class="menu">  
-       <div class="autoriz">
-          $hello
-                <form>$auto_button</form>
-
-       </div>
-              $nav
-       
-       </div>
-   </div>
-</header>
-ROOF;
-
         
-
         
-        $this->roof = $_roof;
-        $this-> content =<<<CONTENT
-            <div class="center-block">
-               <section>
-                   $_content  
-               </section>
-
-            </div> 
-CONTENT;
-        
-        $footer_content = '';//содержимое подвала сайта
-        $socButtonFoot = $socButtonPart1 = $socButtonPart2 = '';
+        $socButtons = '';        
         foreach ($socNets as $value) {
             switch ($value){
-            case 'facebook':
-                //добавляем первую часть кода в начало контента
-                $socButtonPart1  =
-<<<PART1
-    <div id="fb"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.10";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-            
-PART1;
-                   // $this->content = $socButtonPart1.$this->content;
-                    
-                    //формируем вторую часть кода, для добавления в подвал
-                        $socButtonPart2 =
-<<<PART2
-    <div class="fb-like" 
-        data-href="http://www.eng.zzz.com.ua" 
-        data-layout="standard" 
-        data-action="like" 
-        data-size="small" 
-        data-show-faces="true" 
-        data-share="true">
-    </div>            
-PART2;
-                        break;
+                case 'facebook':
+                    $socButtons .= $this->socButtonCodeFacebook();
+
+                            break;
                     
             }
         }
-        //устанавливаем значение подвала
-        $footer_content = <<<FOOTER
-        <footer>
-            <div class="center-block">
-                $socButtonPart1
-               $socButtonPart2 
-            </div>     
-        </footer>
-    </body>
-</html>
-FOOTER;
+        include "/template.php";
+        
+        $this->head = $head;
+        $this->header = $header;
+        $this->content = $t_content;
+        $this->footer = $footer;
 
-        $this-> footer = $footer_content;
+
+        
+
+        
+
+        
+
+
+
     }
     
- 
+    function socButtonCodeFacebook(){
+        //добавляем первую часть кода в начало контента
+        $socButtonPart1  =
+        <<<PART1
+            <div id="fb"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.10";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+            
+PART1;
+        
 
-    
+         //формируем вторую часть кода, для добавления в подвал
+        $socButtonPart2 =
+        <<<PART2
+            <div class="fb-like" 
+                data-href="http://www.eng.zzz.com.ua" 
+                data-layout="standard" 
+                data-action="like" 
+                data-size="small" 
+                data-show-faces="true" 
+                data-share="true">
+            </div>            
+PART2;
+        return $socButtonPart1.$socButtonPart2;
+    }
+                
  
      function build_Page(){
-        return $this -> roof . $this -> content . $this -> footer;
+        $pageHTML = <<<PAGE
+            <!DOCTYPE html>
+            <html>
+                $this->head
+                <body>
+                    $this->header
+                    $this->content
+                    $this->footer
+                </body>
+            </html>
+PAGE;
+        return $pageHTML;
     }
-}
+    }
+    
 
 ?>
---
+++ template.php
@@ -0,0 +1,59 @@
+<?php
+include_once __DIR__ . '/forms/auto_form.php';
+include_once __DIR__ . '/forms/menu_form.php';
+/*
+<!DOCTYPE html>
+<html>
+ */
+$head = <<<HEAD
+<head>
+	<meta charset="utf-8">
+	<title>Read in english</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">   
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+HEAD;
+//<body>
+$header =<<<HEADER
+<header>
+<div class="center-block">
+	<div class="logo">
+		<img src="logo.svg" width="50">
+		<p><i>Читайте с легкостью.</i></p>
+	</div>
+	<div class="menu">	
+		<div class="autoriz">
+                    $auto_form_str
+
+		</div>
+		<nav>
+                    $menu_form_str
+
+		</nav>
+		
+        </div>
+</div>
+</header>
+HEADER;
+$t_content = <<<CONTENT
+<div class="center-block">
+	<section>
+		$_content
+
+	</section>
+	
+</div>
+CONTENT;
+$footer = <<<FOOTER
+	<footer>
+		<div class="center-block">
+			$socButtons
+		</div>		
+	</footer>
+FOOTER;
+/*
+</body>
+</html>
+ * 
+ */
+?>
